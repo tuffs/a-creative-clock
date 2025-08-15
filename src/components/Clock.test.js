ec01647d762abe9408dad2412cdd64db64bc8fad
Devon Kiss <devon@3dmandt.com>
2025-08-15 04:45:08 -0700

    Changes to the way I state what is happening in specs.

--- src/components/Clock.test.js
+++ src/components/Clock.test.js
@@ -6,12 +6,15 @@ describe("Clock Test Suite", () => {
 
   it("renders a clock", () => {
     render(<Clock />);
+
     const clock = screen.getByTestId("clock");
+
     expect(clock).toBeInTheDocument();
   });
 
-  it("renders hours, minutes, and am_or_pm data-testid's", () => {
+  it("displays hours, minutes, and am_or_pm", () => {
     render(<Clock />);
+
     const hours = screen.getByTestId("hours");
     const colon_seperator = screen.getByTestId("colon_seperator");
     const minutes = screen.getByTestId("minutes");
