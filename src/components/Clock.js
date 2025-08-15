9a52b2546ccbb6f39ed1bed0b30363fd30aa0eec
Devon Kiss <devon@3dmandt.com>
2025-08-15 02:33:40 -0700

    Added a bit more formatting and specificity along with a general statement above the time to also try and negate the Arabic text detection with only a numerical output in Arabic numerals.

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -4,7 +4,7 @@ export default function Clock() {
   let minutes = date.getMinutes().toString().padStart(2, '0');
   let am_or_pm = date.getHours() >= 12 ? 'PM' : 'AM';
 
-  const clockDigitStyling = "text-red-500 inline-block text-[7rem] font-bold italic";
+  const clockDigitStyling = "text-red-500 inline-block md:text-[4.75rem] lg:text-[7.25rem] text-[3.75rem] font-bold italic";
 
   return (
     <div
@@ -12,23 +12,36 @@ export default function Clock() {
       id="clock"
       data-testid="clock"
     >
+
       <div
-        className={`${clockDigitStyling}`}
-        data-testid="hours"
+        id="current_time_is__statement"
+        className="text-[13px] md:text-[20px] lg:text-[27px]"
+        style={{
+          fontFamily: 'sans-serif',
+        }}
       >
-        {hours}:
+        The current time is ...
       </div>
-      <div
-        className={`${clockDigitStyling}`}
-        data-testid="minutes"
-      >
-        {minutes}
-      </div>
-      <div
-        className={`${clockDigitStyling}`}
-        data-testid="am_or_pm"
-      >
-        &nbsp;{am_or_pm}
+
+      <div id="clockTimeDisplay" data-testid="clock_time_display">
+        <div
+          className={`${clockDigitStyling}`}
+          data-testid="hours"
+        >
+          {hours}:
+        </div>
+        <div
+          className={`${clockDigitStyling}`}
+          data-testid="minutes"
+        >
+          {minutes}
+        </div>
+        <div
+          className={`${clockDigitStyling}`}
+          data-testid="am_or_pm"
+        >
+          &nbsp;{am_or_pm}
+        </div>
       </div>
     </div>
   );
